336e626af9d3d3d74def4bb2d71c99f8b89591af
Azam Ikram <azamikram22@gmail.com>
2020-05-04 19:20:42 +0500

    Move the code to simple/

--- simple/common.h
+++ simple/common.h
@@ -4,13 +4,13 @@
 #define SEC_TO_MICRO(sec) ((sec) * 1e6)
 #define MICRO_TO_SEC(micro) ((micro) * 1e-6)
 
+#define BYTES_TO_BITS(bytes) ((bytes) * 8)
+#define BYTES_TO_GB(bytes) ((bytes) * 1e-9)
+
 #define SCTP_READ(sockid, msg, len)	 sctp_recvmsg(sockid, msg, len, NULL, 0, NULL, NULL)
 #define SCTP_WRITE(sockid, msg, len) sctp_sendmsg(sockid, msg, len, NULL, 0, 0, 0, 0, 0, 0);
 
 typedef double micro_ts_t;
-
-#define BYTES_TO_BITS(bytes) ((bytes) * 8)
-#define BYTES_TO_GB(bytes) ((bytes) * 1e-9)
 
 // Returns current timestamp in microseconds
 micro_ts_t micro_ts();
